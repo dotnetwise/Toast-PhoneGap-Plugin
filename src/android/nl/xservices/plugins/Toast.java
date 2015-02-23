23f0434a9a8e041fa0154d6bd5518f1060a1a5ef
Laurenţiu Macovei <laurentiu.macovei@dotnetwise.com>
2015-02-23 20:39:34 +0200

    Added closeQueue

--- src/android/nl/xservices/plugins/Toast.java
+++ src/android/nl/xservices/plugins/Toast.java
@@ -1,8 +1,10 @@
 package nl.xservices.plugins;
 
+import android.os.Handler;
 import android.view.Gravity;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.LOG;
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -19,6 +21,7 @@ import org.json.JSONException;
 public class Toast extends CordovaPlugin {
 
   private static final String ACTION_SHOW_EVENT = "show";
+  private static android.widget.Toast _Toast;
 
   private android.widget.Toast mostRecentToast;
 
@@ -36,36 +39,57 @@ public class Toast extends CordovaPlugin {
       final String message = args.getString(0);
       final String duration = args.getString(1);
       final String position = args.getString(2);
+      final String closeQueue = args.length() > 3 ? args.getString(3) : null;
+	  //LOG.d("a", "INSIDE execute(), OUTSIDE run()");
+	  //LOG.d("a", args.toString());
+	  //LOG.d("a", closeQueue != null ? "closeQueue = OK" : "closeQueue NOT OK");
+	  //LOG.d("a", Toast._Toast != null ? "toast = OK" : "toast NOT HERE");
 
       cordova.getActivity().runOnUiThread(new Runnable() {
-        public void run() {
-          android.widget.Toast toast = android.widget.Toast.makeText(webView.getContext(), message, 0);
+          
+  		public void run() {
+		  //LOG.d("a", "INSIDE run()");
+  		  //LOG.d("a", closeQueue != null ? "closeQueue = OK" : "closeQueue NOT OK");
+  		  //LOG.d("a", Toast._Toast != null ? "toast = OK" : "toast NOT HERE");
+  		  if (closeQueue != null && Toast._Toast != null) {// && Toast._Toast.getView() != null && Toast._Toast.getView().isShown()) {
+  			 Toast._Toast.cancel();
+  			 //Toast._Toast.setText(message);
+  		  }
+          
+    		  new Handler().postDelayed(new Runnable() {
+    	    	 	public void run() {
+    	    	          android.widget.Toast toast = android.widget.Toast.makeText(webView.getContext(), message, 0);
+						  Toast._Toast = toast;
 
-          if ("top".equals(position)) {
-            toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 20);
-          } else  if ("bottom".equals(position)) {
-            toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 20);
-          } else if ("center".equals(position)) {
-            toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0, 0);
-          } else {
-            callbackContext.error("invalid position. valid options are 'top', 'center' and 'bottom'");
+    	    	            if ("top".equals(position)) {
+    	    	              toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 20);
+    	    	            } else  if ("bottom".equals(position)) {
+    	    	              toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 20);
+    	    	            } else if ("center".equals(position)) {
+    	    	              toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0, 0);
+    	    	            } else {
+    	    	              callbackContext.error("invalid position. valid options are 'top', 'center' and 'bottom'");
             return;
-          }
+    	    	            }
 
-          if ("short".equals(duration)) {
-            toast.setDuration(android.widget.Toast.LENGTH_SHORT);
-          } else if ("long".equals(duration)) {
-            toast.setDuration(android.widget.Toast.LENGTH_LONG);
-          } else {
-            callbackContext.error("invalid duration. valid options are 'short' and 'long'");
+    	    	            if ("short".equals(duration)) {
+    	    	              toast.setDuration(android.widget.Toast.LENGTH_SHORT);
+    	    	            } else if ("long".equals(duration)) {
+    	    	              toast.setDuration(android.widget.Toast.LENGTH_LONG);
+    	    	            } else {
+    	    	              callbackContext.error("invalid duration. valid options are 'short' and 'long'");
             return;
-          }
+    	    	            }
 
-          toast.show();
+    	    	            toast.show();
           mostRecentToast = toast;
-          callbackContext.success();
-        }
+    	    	            callbackContext.success();
+  	 		
+    	    	 	}
+    	      }, 10);
+    	  }
       });
+    
 
       return true;
     } else {
--- www/Toast.js
+++ www/Toast.js
@@ -1,32 +1,32 @@
 function Toast() {
 }
 
-Toast.prototype.show = function (message, duration, position, successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "Toast", "show", [message, duration, position]);
+Toast.prototype.show = function (message, duration, position, successCallback, errorCallback, closeQueue) {
+	cordova.exec(successCallback, errorCallback, "Toast", "show", [message, duration, position, closeQueue]);
 };
 
-Toast.prototype.showShortTop = function (message, successCallback, errorCallback) {
-  this.show(message, "short", "top", successCallback, errorCallback);
+Toast.prototype.showShortTop = function (message, successCallback, errorCallback, closeQueue) {
+	this.show(message, "short", "top", successCallback, errorCallback, closeQueue);
 };
 
-Toast.prototype.showShortCenter = function (message, successCallback, errorCallback) {
-  this.show(message, "short", "center", successCallback, errorCallback);
+Toast.prototype.showShortCenter = function (message, successCallback, errorCallback, closeQueue) {
+	this.show(message, "short", "center", successCallback, errorCallback, closeQueue);
 };
 
-Toast.prototype.showShortBottom = function (message, successCallback, errorCallback) {
-  this.show(message, "short", "bottom", successCallback, errorCallback);
+Toast.prototype.showShortBottom = function (message, successCallback, errorCallback, closeQueue) {
+	this.show(message, "short", "bottom", successCallback, errorCallback, closeQueue);
 };
 
-Toast.prototype.showLongTop = function (message, successCallback, errorCallback) {
-  this.show(message, "long", "top", successCallback, errorCallback);
+Toast.prototype.showLongTop = function (message, successCallback, errorCallback, closeQueue) {
+	this.show(message, "long", "top", successCallback, errorCallback, closeQueue);
 };
 
-Toast.prototype.showLongCenter = function (message, successCallback, errorCallback) {
-  this.show(message, "long", "center", successCallback, errorCallback);
+Toast.prototype.showLongCenter = function (message, successCallback, errorCallback, closeQueue) {
+	this.show(message, "long", "center", successCallback, errorCallback, closeQueue);
 };
 
-Toast.prototype.showLongBottom = function (message, successCallback, errorCallback) {
-  this.show(message, "long", "bottom", successCallback, errorCallback);
+Toast.prototype.showLongBottom = function (message, successCallback, errorCallback, closeQueue) {
+	this.show(message, "long", "bottom", successCallback, errorCallback, closeQueue);
 };
 
 Toast.install = function () {
